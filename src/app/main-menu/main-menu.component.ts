62f5c75356ed73eb35c238019c74b747e7774d96
LazarMilanov <lazar.milanov.19@singimail.rs>
2022-08-02 01:00:31 +0200

    add ipa button table (WIP)

--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -10,7 +10,56 @@ export class MainMenuComponent implements OnInit {
 
   constructor(private router: Router) { }
 
+  tablesymbols: string[][] = [];
+
+  static symbols = [
+    "p",
+    "t",
+    "k",
+    "b",
+    "d",
+    "g",
+    "a",
+    "ɛ",
+    "e",
+    "i",
+    "ɔ",
+    "o",
+    "u"
+  ];
+
+  readonly rowsize = 6;
+
   ngOnInit(): void {
+
+    let temp: string[] = [];
     
+    for (let i = 0; i < MainMenuComponent.symbols.length; i++) {
+
+      //console.log(MainMenuComponent.symbols[i]);
+      temp.push(MainMenuComponent.symbols[i]);
+      if ( (i + 1) % this.rowsize == 0 || i + 1 == MainMenuComponent.symbols.length ) {
+        this.tablesymbols.push(temp);
+        temp = [];
+      }
+
+    }
+
   }
+
+  ipaClick(event: MouseEvent, arg: string): void {
+      if ((event.target as HTMLButtonElement).classList.contains("btn-primary")) {
+        (event.target as HTMLButtonElement).classList.remove("btn-primary");
+        (event.target as HTMLButtonElement).classList.add("btn-success");
+      } else if ((event.target as HTMLButtonElement).classList.contains("btn-success")) {
+        (event.target as HTMLButtonElement).classList.remove("btn-success");
+        (event.target as HTMLButtonElement).classList.add("btn-primary");
+      } else {
+        console.log("Something happened with button classes");
+      }
+      
+      console.log(arg);
+  }
+  
+
 }
