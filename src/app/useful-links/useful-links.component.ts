dd1d52a0a1cc1b7a51b8adf11fbd22c5488c5845
Sava Jevtić <jevticsava@gmail.com>
2022-08-13 02:13:17 +0200

    begin working on animation (WIP)

--- src/app/useful-links/useful-links.component.ts
+++ src/app/useful-links/useful-links.component.ts
@@ -10,6 +10,23 @@ export class UsefulLinksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.toggled = false;
   }
 
+  toggleSidebar() {
+    (document.getElementById("menu-icon") as HTMLElement).classList.toggle("toggle");
+    if(this.toggled)
+    {
+      (document.getElementById("links-menu") as HTMLElement).classList.remove("toggle");
+      this.toggled = false;
+    }
+    else
+    {
+      this.toggled = true;
+      (document.getElementById("links-menu") as HTMLElement).classList.add("toggle");
+    }
+    console.log((document.getElementById("menu-icon") as HTMLElement).classList + "\n" + (document.getElementById("links-menu") as HTMLElement).classList + "\n" + this.toggled);
+  }
+
+  toggled: boolean;
 }
