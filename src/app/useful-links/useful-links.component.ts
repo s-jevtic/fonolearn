fc1a3fd3752bff9a88de9d61601092481ee72fd4
Sava Jevtić <jevticsava@gmail.com>
2022-09-12 04:47:09 +0200

    reorganize animation code (WIP)

--- src/app/useful-links/useful-links.component.ts
+++ src/app/useful-links/useful-links.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 import {
   animate,
   state,
@@ -6,6 +6,7 @@ import {
   transition,
   trigger
 } from '@angular/animations';
+import { isTokenCharValid } from 'builder-util';
 
 @Component({
   selector: 'app-useful-links',
@@ -20,20 +21,20 @@ import {
         width: 'calc(35px + 3rem)',
       })),
       transition('open <=> closed', [
-        animate('3s ease-in-out'),
+        animate('3s ease-in-out')
       ]),
     ]),
     trigger('changeIcon', [
       state('cross1', style({
         transform: 'rotate(-45deg) translate(-9px, 5px)',
       })),
-      state('cross2', style({
+      state('invisible', style({
         opacity: 0,
       })),
-      state('cross3', style({
+      state('cross2', style({
         transform: 'rotate(45deg) translate(-8px, -5px)'
       })),
-      transition('cross1 <=> menu, cross2 <=> menu, cross3 <=> menu', [
+      transition('cross1 <=> menu, cross2 <=> menu, invisible <=> menu', [
         animate('3s ease-in-out')
       ])
     ])
@@ -44,14 +45,38 @@ export class UsefulLinksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.menuState = 'closed';
+    this.line1State = 'menu';
+    this.line2State = 'menu';
+    this.line3State = 'menu';
+    this.show = false;
     this.toggled = false;
   }
 
   toggleSidebar() {
-    (document.getElementById("sidebar-wrapper") as HTMLElement).classList.toggle("toggle");
+    this.menuState = this.menuState == 'closed'? 'open' : 'closed';
+    this.line1State = this.line1State == 'menu'? 'cross1' : 'menu';
+    this.line2State = this.line2State == 'menu'? 'invisible' : 'menu';
+    this.line3State = this.line3State == 'menu'? 'cross2' : 'menu';
     this.toggled = !this.toggled;
-    // console.log((document.getElementById("menu-icon") as HTMLElement).classList + "\n" + (document.getElementById("links-menu") as HTMLElement).classList + "\n" + this.toggled);
   }
 
+  animationStart(event: AnimationEvent) {
+    if(!this.toggled) {
+      this.show = false;
+    }
+  }
+
+  animationDone(event: AnimationEvent) {
+    if(this.toggled) {
+      this.show = true;
+    }
+  }
+
+  menuState: string;
+  line1State: string;
+  line2State: string;
+  line3State: string;
+  show: boolean;
   toggled: boolean;
 }
