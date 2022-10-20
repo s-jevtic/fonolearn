23b09768a4102668dcfc5a8f40263babd93187ea
Sava Jevtić <jevticsava@gmail.com>
2022-10-21 01:12:56 +0200

    add vowels file (WIP)

---
+++ src/app/phones/vowels.ts
@@ -0,0 +1,19 @@
+export const vowelList = [
+    {
+        "symbol": "ɨ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ə",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "mid",
+        "backness": "central",
+        "clickable": "true",
+        "desc": "mid central vowel (schwa)"
+    }
+]
