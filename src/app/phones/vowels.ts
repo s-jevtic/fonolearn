0fdc34eed11a98dd2dcebd4f8995fd7a68948268
Alexandræ <114509275+Usernamealexandraeisnotavailable@users.noreply.github.com>
2022-10-21 19:30:00 +0200

    Update vowels.ts
    let's hope i didn't mistype anything lol

--- src/app/phones/vowels.ts
+++ src/app/phones/vowels.ts
@@ -1,4 +1,23 @@
 export const vowelList = [
+  
+  
+  
+    {
+        "symbol": "i",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "y",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close",
+        "backness": "front",
+        "clickable": "true"
+    },
     {
         "symbol": "ɨ",
         "voicing": "voiced",
@@ -8,6 +27,111 @@ export const vowelList = [
         "clickable": "true"
     },
     {
+        "symbol": "ʉ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɯ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close",
+        "backness": "back",
+        "clickable": "true"
+    },
+    {
+        "symbol": "u",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close",
+        "backness": "back",
+        "clickable": "true"
+    },
+  
+  
+  
+    {
+        "symbol": "ʊ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "near-close",
+        "backness": "back",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɪ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "near-close",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ʏ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "near-close",
+        "backness": "front",
+        "clickable": "true"
+    },
+  
+  
+  
+    {
+        "symbol": "e",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close-mid",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ø",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close-mid",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɘ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close-mid",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɵ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close-mid",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɤ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "close-mid",
+        "backness": "back",
+        "clickable": "true"
+    },
+    {
+        "symbol": "o",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "close-mid",
+        "backness": "back",
+        "clickable": "true"
+    },
+  
+  
+  
+    {
         "symbol": "ə",
         "voicing": "voiced",
         "roundedness": "unrounded",
@@ -15,5 +139,121 @@ export const vowelList = [
         "backness": "central",
         "clickable": "true",
         "desc": "mid central vowel (schwa)"
+    },
+  
+  
+  
+    {
+        "symbol": "ɛ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open-mid",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "œ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "open-mid",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɜ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open-mid",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɞ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "open-mid",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ʌ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open-mid",
+        "backness": "back",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɔ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "open-mid",
+        "backness": "back",
+        "clickable": "true"
+    },
+  
+  
+  
+    {
+        "symbol": "æ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "near-open",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɐ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "near-open",
+        "backness": "central",
+        "clickable": "true"
+    },
+  
+  
+  
+    {
+        "symbol": "a",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɶ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "open",
+        "backness": "front",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ä",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open",
+        "backness": "central",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɑ",
+        "voicing": "voiced",
+        "roundedness": "unrounded",
+        "height": "open",
+        "backness": "back",
+        "clickable": "true"
+    },
+    {
+        "symbol": "ɒ",
+        "voicing": "voiced",
+        "roundedness": "rounded",
+        "height": "open",
+        "backness": "back",
+        "clickable": "true"
     }
+  
+  
+  
 ]
