850cdc92a3e2dd9d3969b2e096b93b0328a8ea40
Lazar Milanov <93774103+LazarMilanov@users.noreply.github.com>
2022-12-12 02:12:19 +0100

    Useful links sidebar (#6)
    * add sidebar (WIP)
    
    * fix sidebar and bootstrap grid
    
    * begin working on animation (WIP)
    
    * fix errors in sidebar functionality
    
    * minor css adjustments
    
    * rearrange css into separate files
    
    * replace css animation with angular
    
    * improve style
    
    * begin animating menu icon in angular (WIP)
    
    * update modules
    
    * reorganize animation code (WIP)
    
    * update modules
    
    * remove unnecessary package, turn off analytics
    
    * fix menu icon animation
    
    * revert animation to original duration
    
    * make sidebar full height
    
    * add comments and rearrange code
    
    * make sidebar responsive
    
    * fix sidebar height issue
    
    * fix menu icon animation bug
    
    * fix sidebar animation and positioning on mobile
    
    * minor fixes, cleanup, documentation
    
    * cleanup
    
    * attempt to fix responsive sidebar issue (WIP)
    
    * debug animation issue and improve animation
    
    * add swipe feature, minor bugfix
    
    Co-authored-by: Sava Jevtić <sjevtic@protonmail.com>

---
+++ src/app/animations.ts
@@ -0,0 +1,37 @@
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+export const sidebarAnimation = trigger('openClose', [
+    state('open', style({
+      width: 'calc(35px + 16rem)',
+    })),
+    state('open-full', style({
+      width: '100vw'
+    })),
+    state('closed', style({
+      width: 'calc(35px + 3rem)',
+    })),
+    transition('open <=> closed', [
+      animate('0.3s ease-in-out')
+    ]),
+    transition('open-full <=> closed', [
+      animate('0.3s ease-in-out')
+    ]),
+    transition('open-full <=> open', [
+      animate('0.1s ease-in-out')
+    ])
+]);
+
+export const menuIconAnimation = trigger('changeIcon', [
+    state('cross1', style({
+        transform: 'rotate(-45deg) translate(-9px, 5px)',
+    })),
+    state('invisible', style({
+        opacity: 0,
+    })),
+    state('cross2', style({
+        transform: 'rotate(45deg) translate(-8px, -5px)'
+    })),
+    transition('cross1 <=> menu, cross2 <=> menu, invisible <=> menu', [
+        animate('0.3s ease-in-out')
+    ])
+]);
