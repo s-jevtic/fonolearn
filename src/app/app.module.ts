0a997f15d10b437cde765fe6bdeacf20e06f2b4a
Lazar Milanov <93774103+LazarMilanov@users.noreply.github.com>
2023-01-04 23:29:57 +0100

    Develop (#9)
    * run npm install
    
    * fix post merge issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import * as Hammer from 'hammerjs';
 import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 export class MyHammerConfig extends HammerGestureConfig {
   override overrides = <any> {
